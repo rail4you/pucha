997215215c6a856531ca540073009d5bc9caf1b3
zeromovie <714926896@qq.com>
2022-02-16 14:19:46 +0800

    added Hello1

--- api/views.py
+++ api/views.py
@@ -7,8 +7,8 @@ from rest_framework.views import APIView
 
 
 class HelloView(APIView):
-    permission_classes = (IsAuthenticated,)             # <-- And here
+    permission_classes = (IsAuthenticated,)  # <-- And here
 
     def get(self, request):
-        content = {'message': 'Hello, World!'}
+        content = {'message': 'Hello1, World!'}
         return Response(content)
