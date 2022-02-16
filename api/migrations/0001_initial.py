69ba60052457c30904feb3af9278b5f159cc094e
zeromovie <714926896@qq.com>
2022-02-16 16:46:16 +0800

    Code Formatted

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
@@ -17,14 +16,17 @@ class Migration(migrations.Migration):
             fields=[
                 ('password', models.CharField(max_length=128, verbose_name='password')),
                 ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
-                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('is_superuser', models.BooleanField(default=False,
+                                                     help_text='Designates that this user has all permissions without explicitly assigning them.',
+                                                     verbose_name='superuser status')),
                 ('first_name', models.CharField(blank=True, max_length=150, verbose_name='first name')),
                 ('last_name', models.CharField(blank=True, max_length=150, verbose_name='last name')),
                 ('uid', models.IntegerField(primary_key=True, serialize=False)),
                 ('username', models.CharField(max_length=15, unique=True, verbose_name='用户名')),
                 ('nickname', models.CharField(blank=True, max_length=13, null=True, verbose_name='昵称')),
                 ('age', models.IntegerField(blank=True, null=True, verbose_name='年龄')),
-                ('gender', models.CharField(blank=True, choices=[('1', '男'), ('2', '女')], max_length=2, null=True, verbose_name='性别')),
+                ('gender', models.CharField(blank=True, choices=[('1', '男'), ('2', '女')], max_length=2, null=True,
+                                            verbose_name='性别')),
                 ('phone', models.CharField(max_length=11, null=True, verbose_name='手机号码')),
                 ('email', models.EmailField(max_length=254, verbose_name='邮箱')),
                 ('home_address', models.CharField(blank=True, max_length=100, null=True, verbose_name='地址')),
@@ -32,8 +34,13 @@ class Migration(migrations.Migration):
                 ('is_active', models.BooleanField(default=True, verbose_name='激活状态')),
                 ('is_staff', models.BooleanField(default=True, verbose_name='是否是员工')),
                 ('date_joined', models.DateTimeField(auto_now_add=True)),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.Group', verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.Permission', verbose_name='user permissions')),
+                ('groups', models.ManyToManyField(blank=True,
+                                                  help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+                                                  related_name='user_set', related_query_name='user', to='auth.Group',
+                                                  verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.',
+                                                            related_name='user_set', related_query_name='user',
+                                                            to='auth.Permission', verbose_name='user permissions')),
             ],
             options={
                 'verbose_name': '用户',
