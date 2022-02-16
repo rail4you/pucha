4fa47c28d33bcbaa3e5e31e7e5051e053ccc7726
baibai <rail4you@gmail.com>
2022-02-16 16:21:06 +0800

    add custom user model

--- api/models.py
+++ api/models.py
@@ -49,7 +49,7 @@ class User(AbstractUser): # 自定义User
         ("2","女")
     )
     uid = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=15,verbose_name="用户名",unique=True)
+    username = models.CharField(max_length=15,verbose_name="用户名")
     nickname = models.CharField(max_length=13,verbose_name="昵称",null=True,blank=True)
     age = models.IntegerField(verbose_name="年龄",null=True,blank=True)
     gender = models.CharField(max_length=2,choices=GENDER_TYPE,verbose_name="性别",null=True,blank=True)
@@ -57,12 +57,12 @@ class User(AbstractUser): # 自定义User
     email = models.EmailField(verbose_name="邮箱")
     # picture = models.ImageField(upload_to="Store/user_picture",verbose_name="用户头像",null=True,blank=True)
     home_address = models.CharField(max_length=100,null=True,blank=True,verbose_name="地址")
-    card_id = models.CharField(max_length=30,verbose_name="身份证")
+    card_id = models.CharField(max_length=30,verbose_name="身份证", unique=True)
     is_active = models.BooleanField(default=True,verbose_name="激活状态")
     is_staff = models.BooleanField(default=True,verbose_name="是否是员工")
     date_joined = models.DateTimeField(auto_now_add=True)
 
-    USERNAME_FIELD = 'username' # 使用authenticate验证时使用的验证字段，可以换成其他字段，但验证字段必须是唯一的，即设置了unique=True
+    USERNAME_FIELD = 'card_id' # 使用authenticate验证时使用的验证字段，可以换成其他字段，但验证字段必须是唯一的，即设置了unique=True
     REQUIRED_FIELDS = ['email'] # 创建用户时必须填写的字段，除了该列表里的字段还包括password字段以及USERNAME_FIELD中的字段
     EMAIL_FIELD = 'email' # 发送邮件时使用的字段
 
